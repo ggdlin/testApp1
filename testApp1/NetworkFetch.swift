ff8eff3da33d9fd8341c51cabc6ce11034b96423
Sergey Ivanov <sergey.ivanov@orioninc.com>
2021-08-30 09:42:41 +0300

    ok

--- testApp1/NetworkFetch.swift
+++ testApp1/NetworkFetch.swift
@@ -15,42 +15,34 @@ class NetworkFetch: Fetcher {
     
     func fetch(itemsAmount: Int) {
         guard let imageJsonURL = URL(string: imagePath) else { print("Bad image url"); return }
-        
-        
-        
-        
-        
         for requestIndex in 0..<itemsAmount {
-            DispatchQueue.global(qos: .background).async { [weak self] in
-                let image = self?.fetchImage() ?? UIImage(systemName: "exclamationmark.shield")!
-                let text = self?.fetchQuote()
-                let item = ImageAndText(image: image, text: text)
-                print("image \(requestIndex) of \(itemsAmount) fetched")
-                self?.delegate?.fetcher(asyncReceivedItem: item)
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            
+            
+                URLSession.shared.dataTask(with: imageJsonURL) { (data, _, error) in
+                    if let error = error { print(error); return }
+                    if let imageJsonData = data {
+                        guard let parsedStringImageURL = try? JSONDecoder().decode(RandomImageJson.self, from: imageJsonData) else { return }
+                        guard let imageURL = URL(string: parsedStringImageURL.url) else { return }
+                        
+                        URLSession.shared.dataTask(with: imageURL) { (data, _, error) in
+                            if let error = error { print(error); return }
+                            if let imageData = data, let image = UIImage(data: imageData) {
+                                let text = self?.fetchQuote()
+                                let item = ImageAndText(image: image, text: text)
+                                print("image \(requestIndex) of \(itemsAmount) fetched")
+                                self?.delegate?.fetcher(asyncReceivedItem: item)
+                            }
+                            
+                        }.resume()
+                        
+                    }
+                }.resume()
             }
         }
     }
     
-    private func fetchImage() -> UIImage? {
-        
-        
-//        guard let imageJsonData = try? Data(contentsOf: imageJsonURL) else { return nil }
 
-        URLSession.shared.dataTask(with: imageJsonURL) { (data, _, error) in
-            guard let imageJsonData = data else { print("error: \(error?.localizedDescription ?? "unknown")"); return }
-            guard let parsedStringImageURL = try? JSONDecoder().decode(RandomImageJson.self, from: imageJsonData) else { return nil }
-            
-        }.resume()
-        
-        
-        guard let imageURL = URL(string: parsedStringImageURL.url) else { return nil }
-        guard let imageData = try? Data(contentsOf: imageURL) else { return nil }
-        guard let image = UIImage(data: imageData) else { return nil }
-        
-        
-        return image
-    }
-    
     private func fetchQuote() -> String? {
         guard let textJsonURL = URL(string: textPath) else { print("Bad text url"); return nil }
         guard let textJsonData = try? Data(contentsOf: textJsonURL) else { return nil }
